0c3df56b96f6f4499d94e55ee85699468fc39fa3
zzg <1804124963@qq.com>
2018-08-20 23:41:46 +0800

    RecycleView

---
+++ app/src/main/java/com/example/myappsecond/activity/TreeListActivity.java
@@ -0,0 +1,30 @@
+package com.example.myappsecond.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.example.myappsecond.BaseActivity;
+import com.example.myappsecond.R;
+
+/**
+ * Created by Zzg on 2018/8/20.
+ */
+public class TreeListActivity extends BaseActivity {
+    RecyclerView recyclerView;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tree_list);
+        init();
+    }
+
+    private void init() {
+        setBarTitle("树");
+        recyclerView=findViewById(R.id.recyclerView);
+        LinearLayoutManager manager=new LinearLayoutManager(this);
+        manager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(manager);
+    }
+}

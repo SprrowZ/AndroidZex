a245327f7229156d0775cfcb2eff18e7bee46340
SprrowZ <zhaoguge2@gmail.com>
2019-01-13 23:32:40 +0800

    提交

--- app/src/main/java/com/rye/catcher/project/ctmviews/zPullToRefreshView.java
+++ app/src/main/java/com/rye/catcher/project/ctmviews/zPullToRefreshView.java
@@ -3,12 +3,16 @@ package com.rye.catcher.project.ctmviews;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
+import android.widget.Scroller;
 
 import com.rye.catcher.R;
+import com.rye.catcher.utils.DensityUtil;
 import com.rye.catcher.utils.MeasureUtil;
 
 /**
@@ -18,15 +22,21 @@ import com.rye.catcher.utils.MeasureUtil;
  * @function:
  */
 public class zPullToRefreshView extends ViewGroup {
+    private static final String TAG="zPullToRefreshView";
     /**
      * 上下加载视图
      */
+    //头部、底部
     private View zHeaderView;
     private View zBottomView;
     private View mainView;
-    private int mHeight;
-    private int mWidth;
-    private int contentHeight= MeasureUtil.getScreenHeight(getContext());
+    //此ViewGroup的高宽
+    private int mHeight=-1;
+    private int mWidth=-1;
+    //主视图高度
+    private int contentHeight;
+
+    private Scroller mScroller;
     public zPullToRefreshView(Context context) {
         this(context,null);
     }
@@ -37,19 +47,23 @@ public class zPullToRefreshView extends ViewGroup {
 
     public zPullToRefreshView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        //先实例化头部尾部布局
         zHeaderView= LayoutInflater.from(getContext()).inflate(R.layout.top,null,false);
         zBottomView= LayoutInflater.from(getContext()).inflate(R.layout.bottom,null,false);
+        //滑动
+        mScroller=new Scroller(context);
     }
 
     @Override
     protected void onFinishInflate() {
         super.onFinishInflate();
         RelativeLayout.LayoutParams params=new RelativeLayout.LayoutParams(LayoutParams.MATCH_PARENT,
-                LayoutParams.MATCH_PARENT);
+                LayoutParams.WRAP_CONTENT);
        zHeaderView.setLayoutParams(params);
        zBottomView.setLayoutParams(params);
        addView(zHeaderView);
        addView(zBottomView);
+       contentHeight=MeasureUtil.getScreenHeight(getContext());
     }
 
     @Override
@@ -58,10 +72,16 @@ public class zPullToRefreshView extends ViewGroup {
         int count=getChildCount();
 
         for (int i=0;i<count;i++){
-            mHeight+=getChildAt(i).getHeight();
+            View child=getChildAt(i);
+            measureChild(child,widthMeasureSpec,heightMeasureSpec);
+            mHeight+=child.getMeasuredHeight();
+            if (child.getMeasuredWidth()>mWidth){
+                mWidth=child.getMeasuredWidth();
+            }
         }
+        int finalWidth=resolveSize(mWidth,widthMeasureSpec);
         int finalHeight=resolveSize(mHeight,heightMeasureSpec);
-        setMeasuredDimension(widthMeasureSpec,finalHeight);
+        setMeasuredDimension(finalWidth,finalHeight);
     }
 
     @Override
@@ -73,15 +93,58 @@ public class zPullToRefreshView extends ViewGroup {
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
       int count=getChildCount();
        for (int i=0;i<count;i++){
-           View view=getChildAt(i);
-           if (view==zHeaderView){
-               layout(0,-view.getHeight(),view.getWidth(),0);
-           }else if (view==zBottomView){
-               layout(0,contentHeight,view.getWidth(),contentHeight+view.getHeight());
+           View child=getChildAt(i);
+           if (child==zHeaderView){
+               child.layout(0,-100,child.getMeasuredWidth(),0);
+           }else if (child==zBottomView){
+               child.layout(0,contentHeight,child.getWidth(),contentHeight+100);
            }else{
-              layout(0,0,MeasureUtil.getScreenWidth(getContext()),
-                      MeasureUtil.getScreenHeight(getContext()));
+              child.layout(0,0,child.getMeasuredWidth(),child.getMeasuredHeight());
            }
        }
     }
+
+    private int mLastMoveY;
+    private int originY;
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int y = (int) event.getY();
+        Log.i(TAG, "onTouchEvent: yyyy"+y);
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                mLastMoveY = y;
+                originY=y;
+                Log.i(TAG, "onTouchEvent:cccc ");
+                break;
+            case MotionEvent.ACTION_MOVE:
+                int dy = mLastMoveY - y;
+                if (Math.abs(originY-y)>200){
+                    break;
+                }
+                Log.i(TAG, "onTouchEvent: "+dy);
+                scrollBy(0, dy);
+                break;
+            case MotionEvent.ACTION_UP:
+                //方向要搞清
+                int wholeDistance=y-originY;
+                if (Math.abs(wholeDistance)>200){
+                    if (wholeDistance>0){//向上滑动
+                        scrollBy(0,200);
+                    }else{
+                        scrollBy(0,-200);
+                    }
+                     break;
+                }
+                scrollBy(0,wholeDistance);
+                break;
+        }
+
+        mLastMoveY = y;
+        Log.i(TAG, "onTouchEvent: xxxxxxx");
+        //不要忘了返回为true消耗此次事件
+        return true;
+    }
+
+
+
 }

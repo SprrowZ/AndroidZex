81199d34b5aba3f44aa7947553876862dbf78118
SprrowZ <33713300+SprrowZ@users.noreply.github.com>
2019-04-11 19:12:03 +0800

    提交

---
+++ app/src/main/java/com/rye/catcher/activity/MultiDemoFragment.java
@@ -0,0 +1,22 @@
+package com.rye.catcher.activity;
+
+import com.rye.catcher.R;
+import com.rye.catcher.activity.fragment.BaseFragment;
+
+/**
+ * Created at 2019/4/10.
+ *
+ * @author Zzg
+ * @function:
+ */
+public class MultiDemoFragment extends BaseFragment {
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.fragment_multi_demo;
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
--- app/src/main/java/com/rye/catcher/project/ctmviews/takephoto/TestPhotoActivity.java
+++ app/src/main/java/com/rye/catcher/project/ctmviews/takephoto/TestPhotoActivity.java
@@ -3,9 +3,14 @@ package com.rye.catcher.project.ctmviews.takephoto;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -13,9 +18,16 @@ import android.view.View;
 import android.widget.ImageView;
 
 import com.rye.catcher.R;
+import com.rye.catcher.utils.SDHelper;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class TestPhotoActivity extends AppCompatActivity {
     private ImageView imageView;
+
+    private static final int CAMERA_REQUEST_CODE=99;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +43,11 @@ public class TestPhotoActivity extends AppCompatActivity {
             if (!TextUtils.isEmpty(path)) {
                 imageView.setImageBitmap(BitmapFactory.decodeFile(path));
             }
+        }else if (requestCode==CAMERA_REQUEST_CODE&&resultCode==RESULT_OK){
+            if (data!=null){
+                imageView.setImageBitmap(BitmapFactory.decodeFile(getImagePath()));
+            }
+
         }
     }
 
@@ -50,8 +67,28 @@ public class TestPhotoActivity extends AppCompatActivity {
     /**
      * 身份证
      */
-    public void takeID(View view) {
-        takePhoto(CameraActivity2.TYPE_ID);
+    public void takePhoto(View view) {
+        //拍照页面
+         Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+         File file=new File(getImagePath());
+         Uri uri;
+         //低于7.0用file://
+         if (Build.VERSION.SDK_INT<Build.VERSION_CODES.N){
+               uri=Uri.fromFile(file);
+         }else{
+             uri= FileProvider.getUriForFile(this,this.getApplicationContext().getPackageName()+".provider",file);
+         }
+         intent.putExtra(MediaStore.EXTRA_OUTPUT,uri);
+         startActivityForResult(intent,CAMERA_REQUEST_CODE);
+
+    }
+
+    private String getImagePath(){
+        Date data=new Date();
+        SimpleDateFormat sdf=new SimpleDateFormat("yy-MM-DD%HH:mm:ss");
+        String dataStr=sdf.format(data);
+        String imagePath=SDHelper.getImageFolder()+dataStr+".png";
+        return imagePath;
     }
 
     /**
@@ -74,4 +111,6 @@ public class TestPhotoActivity extends AppCompatActivity {
     public void takeLicense(View view) {
         takePhoto(CameraActivity2.TYPE_LICENSE);
     }
+
+
 }
